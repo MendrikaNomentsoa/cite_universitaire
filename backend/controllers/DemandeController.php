68baf4ab5aadaf1e4c1b9807d0e3cd1990772ccc
wallis <wallisramampiandra@gmail.com>
2026-05-08 15:40:50 +0300

    ajout du modele gerant la table etudiant dans demandeController

--- backend/controllers/DemandeController.php
+++ backend/controllers/DemandeController.php
@@ -3,12 +3,13 @@
 
 require_once __DIR__ . '/../models/DemandeModel.php';
 require_once __DIR__ . '/../utils/Response.php';
-
+require_once __DIR__. '/../models/EtudiantModel.php';
 class DemandeController {
     private DemandeModel $model;
-
+    private EtudiantModel $etudiant;
     public function __construct() {
         $this->model = new DemandeModel();
+        $this->etudiant=new EtudiantModel();
     }
 
     // GET /demandes
@@ -27,7 +28,14 @@ class DemandeController {
     public function store(): void {
         $data = json_decode(file_get_contents('php://input'), true) ?? [];
         $id = $this->model->create($data);
-        Response::success(['numDemande' => $id], 'Demande créée.');
+    //ajout de l'information de l'etudiant
+        foreach (['nomEtudiant', 'prenoms', 'anneeNaissance', 'niveau', 'sexe', 'numEcole'] as $champ) {
+            if (empty($data[$champ])) Response::error("Champ obligatoire : $champ");
+        }
+        $numEtudiant=$this->etudiant->create($data);
+    //ajout de la relation entre l'etudiant et l'ecole
+        
+        Response::success(['numDemande' => $id,'numEtudiant'=>$numEtudiant], 'Demande créée.');
     }
 
     // PUT /demandes/{id}
